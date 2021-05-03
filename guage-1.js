d715ac779ce74322b6b5b4f29a10bd8a49215c89
Rashid Baset <rashid.baset@gmail.com>
2021-05-02 21:12:03 -0400

    Update guage-1.js

--- guage-1.js
+++ guage-1.js
@@ -1,36 +1,9 @@
 {
-  "item":[
-    {
-      "value":"87809",
-      "label":"Step1"
-    },
-    {
-      "value":"70022",
-      "label":"Step2"
-    },
-    {
-      "value":"63232",
-      "label":"Step 3"
-    },
-    {
-      "value":"53232",
-      "label":"Step 4"
-    },
-    {
-      "value":"32123",
-      "label":"Step 5"
-    },
-    {
-      "value":"23232",
-      "label":"Step 6"
-    },
-    {
-      "value":"12232",
-      "label":"Step 7"
-    },
-    {
-      "value":"2323",
-      "label":"Step 8"
-    }
-  ]
+  "item": 23,
+  "min": {
+    "value": 10
+  },
+  "max": {
+    "value": 30
+  }
 }
