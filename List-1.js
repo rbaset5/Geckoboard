a9b804ff0d509f516d438c0afd20dff9b6afee04
Rashid Baset <rashid.baset@gmail.com>
2021-05-02 22:32:34 -0400

    Create List-1.js

---
+++ List-1.js
@@ -0,0 +1,36 @@
+[
+  {
+    "title": {
+      "text": "Chrome"
+    },
+    "label": {
+      "name": "New!",
+      "color": "#ff2015"
+    },
+    "description": "40327 visits"
+  },
+  {
+    "title": {
+      "text": "Safari"
+    },
+    "description": "11577 visits"
+  },
+  {
+    "title": {
+      "text": "Firefox"
+    },
+    "description": "10296 visits"
+  },
+  {
+    "title": {
+      "text": "Internet Explorer"
+    },
+    "description": "3587 visits"
+  },
+  {
+    "title": {
+      "text": "Opera"
+    },
+    "description": "499 visits"
+  }
+]
