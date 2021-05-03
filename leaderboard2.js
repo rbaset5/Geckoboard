1c4c90f287a3f0e86e77cac7ccd065d5acee56c1
Rashid Baset <rashid.baset@gmail.com>
2021-05-02 22:44:37 -0400

    Create leaderboard2.js

---
+++ leaderboard2.js
@@ -0,0 +1,66 @@
+{
+  "items": [
+    {
+      "label": "Peter",
+      "value": 234,
+      "previous_rank": 2
+    },
+    {
+      "label": "Patrick",
+      "value": 232,
+      "previous_rank": 1
+    },
+    {
+      "label": "Jon",
+      "value": 230
+    },
+    {
+      "label": "Clara",
+      "value": 220
+    },
+    {
+      "label": "Tom",
+      "value": 215,
+      "previous_rank": 7
+    },
+    {
+      "label": "Sylvester",
+      "value": 200
+    },
+    {
+      "label": "David",
+      "value": 195,
+      "previous_rank": 5
+    },
+    {
+      "label": "Matt",
+      "value": 190
+    },
+    {
+      "label": "William",
+      "value": 185
+    },
+    {
+      "label": "Rose",
+      "value": 182,
+      "previous_rank": 11
+    },
+    {
+      "label": "Colin",
+      "value": 178,
+      "previous_rank": 10
+    },
+    {
+      "label": "Paul",
+      "value": 172
+    },
+    {
+      "label": "Amy",
+      "value": 166
+    },
+    {
+      "label": "Christopher",
+      "value": 162
+    }
+  ]
+}
