6754dab2275f94a8a0f553b5c3e548bcdcdb3f43
Rashid Baset <rashid.baset@gmail.com>
2021-05-02 21:19:41 -0400

    Create bullet-graph-1.js

---
+++ bullet-graph-1.js
@@ -0,0 +1,37 @@
+{
+  "orientation": "horizontal",
+  "item": {
+    "label": "Revenue 2014 YTD",
+    "sublabel": "(U.S. $ in thousands)",
+    "axis": {
+      "point": ["0", "200", "400", "600", "800", "1000"]
+    },
+    "range": {
+      "red": {
+        "start": 0,
+        "end": 400
+      },
+      "amber": {
+        "start": 401,
+        "end": 700
+      },
+      "green": {
+        "start": 701,
+        "end": 1000
+      }
+    },
+    "measure": {
+      "current": {
+        "start": "0",
+        "end": "500"
+      },
+      "projected": {
+        "start": "100",
+        "end": "900"
+      }
+    },
+    "comparative": {
+      "point": "600"
+    }
+  }
+}
