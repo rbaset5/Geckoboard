438386d188dd55eace77eee4ac2912c231555fa0
Rashid Baset <rashid.baset@gmail.com>
2021-05-03 06:51:21 -0400

    Create formattedvalues-2.js

---
+++ formattedvalues-2.js
@@ -0,0 +1,46 @@
+{
+  "format": "currency",
+  "unit": "GBP",
+  "items": [
+    {
+      "label": "Peter",
+      "value": 99
+    },
+    {
+      "label": "Patrick",
+      "value": 98.5
+    },
+    {
+      "label": "Jon",
+      "value": 97.0
+    },
+    {
+      "label": "Clara",
+      "value": 96.5
+    },
+    {
+      "label": "Tom",
+      "value": 96.3
+    },
+    {
+      "label": "Sylvester",
+      "value": 96.1
+    },
+    {
+      "label": "David",
+      "value": 96.0
+    },
+    {
+      "label": "Matt",
+      "value": 95
+    },
+    {
+      "label": "William",
+      "value": 95
+    },
+    {
+      "label": "Rose",
+      "value": 94
+    }
+  ]
+}
