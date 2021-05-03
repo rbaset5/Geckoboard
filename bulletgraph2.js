71aa9dfc1705acc0250ab6309d8d93ff0a553b30
Rashid Baset <rashid.baset@gmail.com>
2021-05-02 22:46:11 -0400

    Create bulletgraph2.js

---
+++ bulletgraph2.js
@@ -0,0 +1,153 @@
+{
+  "orientation": "vertical",
+  "item": [
+    {
+      "axis": {
+        "point": [
+          "0",
+          "50",
+          "100",
+          "150",
+          "200",
+          "250"
+        ]
+      },
+      "comparative": {
+        "point": "220"
+      },
+      "label": "Revenue",
+      "measure": {
+        "current": {
+          "end": "235",
+          "start": "0"
+        }
+      },
+      "range": {
+        "red": {
+          "end": "125",
+          "start": "0"
+        },
+        "amber": {
+          "end": "200",
+          "start": "126"
+        },
+        "green": {
+          "end": "250",
+          "start": "201"
+        }
+      },
+      "sublabel": "U.S. $ (1,000s)"
+    },
+    {
+      "axis": {
+        "point": [
+          "0",
+          "30",
+          "60",
+          "90",
+          "120",
+          "150"
+        ]
+      },
+      "comparative": {
+        "point": "80"
+      },
+      "label": "Expenses",
+      "measure": {
+        "current": {
+          "end": "65",
+          "start": "0"
+        }
+      },
+      "range": {
+        "red": {
+          "end": "150",
+          "start": "91"
+        },
+        "amber": {
+          "end": "90",
+          "start": "51"
+        },
+        "green": {
+          "end": "50",
+          "start": "0"
+        }
+      },
+      "sublabel": "U.S. $ (1,000s)"
+    },
+    {
+      "axis": {
+        "point": [
+          "0",
+          "100",
+          "200",
+          "300",
+          "400",
+          "500"
+        ]
+      },
+      "comparative": {
+        "point": "450"
+      },
+      "label": "Avg Order",
+      "measure": {
+        "current": {
+          "end": "230",
+          "start": "0"
+        }
+      },
+      "range": {
+        "red": {
+          "end": "250",
+          "start": "0"
+        },
+        "amber": {
+          "end": "400",
+          "start": "251"
+        },
+        "green": {
+          "end": "500",
+          "start": "401"
+        }
+      },
+      "sublabel": "U.S. $"
+    },
+    {
+      "axis": {
+        "point": [
+          "0",
+          "1",
+          "2",
+          "3",
+          "4",
+          "5"
+        ]
+      },
+      "comparative": {
+        "point": "4.3"
+      },
+      "label": "Cust Sat",
+      "measure": {
+        "current": {
+          "end": "4.6",
+          "start": "0"
+        }
+      },
+      "range": {
+        "red": {
+          "end": "3.3",
+          "start": "0"
+        },
+        "amber": {
+          "end": "4.25",
+          "start": "3.4"
+        },
+        "green": {
+          "end": "5",
+          "start": "4.26"
+        }
+      },
+      "sublabel": "Top Rating of 5"
+    }
+  ]
+}
